4395fafb8bf7dec32b06f83aa492b21519e62b34
jmccormac01 <jmccormac001@gmail.com>
2016-03-24 15:32:10 +0000

    initial commit of updated QueryNOTdata.py

---
+++ QueryNOTdata.py
@@ -0,0 +1,44 @@
+
+# ----------------------------------------------------------------------------------
+#								Description
+# ----------------------------------------------------------------------------------
+#
+# QueryNOTdata.py - 	a program to make a list of targets observed from NOT data 
+#	
+#							Version 1.00 (22/11/12)
+#
+
+# ----------------------------------------------------------------------------------
+# 								Update History
+# ----------------------------------------------------------------------------------
+# 05/06/09 - 	code writen
+# 05/06/09 -	code tested
+#
+# 11 Gb raw NOT data zipped
+#
+
+import os
+import glob as g
+from astropy.io import fits
+from collections import defaultdict
+
+top_dir=os.getcwd()
+t=g.glob('NOT_20*')
+target=defaultdict(list)
+
+for j in range(0,len(t)):
+	os.chdir('%s/fies/' % (t[j]))
+	t2=g.glob('FI*.fits')
+	for i in range(0,len(t2)):
+		with fits.open(l[i]) as hdu:
+			obj_id=hdu[0].header['TCSTGT']
+			target[obj_id].append(t2[i])
+	os.chdir('../')
+				
+
+
+#f=open('%s/ObjectsObserved.txt' % (top_dir),'w')
+#for i in range(0,len(tar_list_n)):
+#	f.write("%s    %s\n" % (tar_list_n[i],tar_count_n[i]))
+#	
+#f.close()
